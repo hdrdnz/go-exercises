e67f1f1481c19234f2265a1d74c4e325abece956
hdrdnz <h_durudeniz@hotmail.com>
2023-05-12 14:29:48 +0300

    add import

---
+++ import/main.go
@@ -0,0 +1,11 @@
+package main
+
+// fmt paketini f isminde kullanmamıza olanak sağlar. fmt.Println() yazmak yerine f.Println() yazacağız.
+// import f "fmt"
+
+//import kelimesinden sonra nokta koyduk. Bu işlem sayesinde fmt.Println() yazmak yerine sadece Println() yazarak aynı işi yapmış oluruz.
+import . "fmt"
+
+func main() {
+	Println("hey")
+}
